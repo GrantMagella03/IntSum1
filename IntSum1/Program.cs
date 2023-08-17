80db29bad8c0e2854386bdae0938a64dc190c4ff
Grant Magella <Bitman2003@gmail.com>
2023-08-17 14:21:15 -0400

    Add project files.

---
+++ IntSum1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float[] nbrs = { 9, 1, 6, 9, 2, 4, 5, 3, 1, 3 };
+        int index = 0;
+        float sum = 0;
+        while (index < 10)
+        {
+            sum += nbrs[index];
+            index++;
+        }
+        Console.WriteLine(sum);
+        float avg = 0;
+        avg = sum / 10; 
+        Console.WriteLine(avg);
+    }
+}
