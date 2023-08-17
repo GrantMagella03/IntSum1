d379c85604924cb750d541b69ca8ba36b49bcde6
Grant Magella <Bitman2003@gmail.com>
2023-08-17 15:42:26 -0400

    m

--- IntSum1/Program.cs
+++ IntSum1/Program.cs
@@ -3,16 +3,20 @@
     private static void Main(string[] args)
     {
         float[] nbrs = { 9, 1, 6, 9, 2, 4, 5, 3, 1, 3 };
-        int index = 0;
         float sum = 0;
-        while (index < 10)
+        float oddsum = 0;
+        for (int idx=0; idx<nbrs.Length; idx++)
         {
-            sum += nbrs[index];
-            index++;
+            sum += nbrs[idx];
+            if (nbrs[idx]%2==1)
+            {
+                oddsum += nbrs[idx] * nbrs[idx];
+            }
         }
-        Console.WriteLine(sum);
+        Console.WriteLine($"Sum:"+sum);
+        Console.WriteLine($"Oddsum^2: "+oddsum);
         float avg = 0;
-        avg = sum / 10; 
+        avg = sum / nbrs.Length; 
         Console.WriteLine(avg);
     }
 }
